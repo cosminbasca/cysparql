49cc0be29acf42980bc6da618cc4cfc0d20677e2
Cosmin Basca <cosmin.basca@gmail.com>
2013-12-19 20:18:17 +0000

    bumped to 0.5.14
    - query exception

--- debug.py
+++ debug.py
@@ -1,23 +1,25 @@
 __author__ = 'basca'
 
 from cysparql import *
-
-w = RasqalWorld()
-literals = [
-    QueryLiteral.new_typed_literal(w, LiteralType.BOOLEAN, "1"),
-    QueryLiteral.new_string_literal(w, "muhaha"),
-    QueryLiteral.new_string_literal(w, "muhaha", lang="en"),
-    QueryLiteral.new_string_literal(w, "45.89", dtype="http://www.w3.org/2001/XMLSchema#float"),
-]
-for tlit in literals:
-    print '------------------------------------------------------------------------------------'
-    print tlit, tlit.literal_type_label
-    print tlit.as_str(), ' -----> ',tlit.to_python().n3()
-
+#
+# w = RasqalWorld()
+# literals = [
+#     QueryLiteral.new_typed_literal(w, LiteralType.BOOLEAN, "1"),
+#     QueryLiteral.new_string_literal(w, "muhaha"),
+#     QueryLiteral.new_string_literal(w, "muhaha", lang="en"),
+#     QueryLiteral.new_string_literal(w, "45.89", dtype="http://www.w3.org/2001/XMLSchema#float"),
+# ]
+# for tlit in literals:
+#     print '------------------------------------------------------------------------------------'
+#     print tlit, tlit.literal_type_label
+#     print tlit.as_str(), ' -----> ',tlit.to_python().n3()
+#
 
 
 Q = """
 PREFIX foaf: <http://xmlns.com/foaf/>
+PREFIX XXX: <http://xmlns.com/foafadsads/>
+PREFIX YYY: <http://xmlns.com/foaf4525234/>
 PREFIX example: <http://www.example.org/rdf#>
 SELECT ?var_0 ?var_2 WHERE {
     ?var_0 foaf:knows ?var_2 .
